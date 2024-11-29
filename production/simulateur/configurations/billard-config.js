450e5ae0df3749c719be29a7c97e5bd54adc697d
Antoine FONTANEZ <antoinef252@gmail.com>
2024-11-29 14:54:51 +0100

    Ajout des boules cerclées à la configuration triangulaire

--- production/simulateur/configurations/billard-config.js
+++ production/simulateur/configurations/billard-config.js
@@ -24,17 +24,18 @@ class BillardConfig extends Table {
         let nbCol = 5;
 
         // White ball
-        balls.push(new Ball(ballRadius, ballColors[7], width / 2, height / 2));
+        balls.push(new Ball(ballRadius, "white", width / 2, height / 2));
 
         for (let i = 0; i <= nbCol; i++) {
             for (let j = 0; j < i; j++) {
                 let ball = new Ball(ballRadius, ballColors[
                     Math.floor(Math.random() * 7)
-                    ], x, y);
+                    ], x, y, (Math.random() * 2) < 1);
                 balls.push(ball);
 
                 y += ballRadius * 2;
             }
+
             y = height / 2 - (i * ballRadius);
             x += ballRadius * 2 - 3; // pour se faire rapprocher les balles
         }
