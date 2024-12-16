33ac54c44f577373a6c8981c69c7cadfd3b87af3
Corentin <corentin.froger@netc.fr>
2024-12-16 09:02:33 +0100

    Nettoyage server.js - code commenté enlevé - Commentaires ajoutés - variable port introduite

--- production/server.js
+++ production/server.js
@@ -2,42 +2,37 @@ const path=require("path");
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-//io = require('socket.io').listen(server, {
 
 const io = require('socket.io').listen(server, {
-    //path: '/test',
-    //serveClient: false,
-    // below are engine.IO options
     pingInterval: 30000,
     pingTimeout: 5000,
-    //cookie: false
 });
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+const port = 8001;
 
-server.listen(8001);
-
+// - Utilisé pour gérer des problèmes de répertoire -----------------
 app.use(express.static(path.join(__dirname)));
 
-io.sockets.on('connection', function (socket) {
-    console.log('Socket connected: ' + socket.conn.remoteAddress);
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
 
-    socket.on('motor', function(val) {
-        console.log('motor'+JSON.stringify(val));
-        socket.broadcast.emit('motor', val);
+// - Affichage en continu des sockets qui passent -------------------
+server.listen(port);
+
+// Lorsqu'un ordre est envoyé au robot depuis le navigateur,
+// il passe par le serveur, puis est envoyé au(x) robot(s)
+io.sockets.on("connection", function (socket) {
+    console.log("Socket connected: " + socket.conn.remoteAddress);
+
+    socket.on("motor", function(val) {
+        console.log("motor" + JSON.stringify(val));
+        socket.broadcast.emit("motor", val);
     });
 
-    socket.on('disconnect', function() {
-        console.log('Socket disconnected');
+    socket.on("disconnect", function() {
+        console.log("Socket disconnected");
     });
 });
 
-
-
-
-console.log("Server is running.");
-
-
-
+console.log(`Server is running on port ${port}`);
