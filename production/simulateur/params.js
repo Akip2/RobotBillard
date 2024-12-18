720431d6f65fa4bcd6b1a58c27bc4e3ad94ee17c
Antoine FONTANEZ <antoinef252@gmail.com>
2024-12-18 22:21:09 +0100

    SIMULATEUR: Mise à l'échelle

--- production/simulateur/params.js
+++ production/simulateur/params.js
@@ -1,9 +1,9 @@
-const width = 700;
-const height = 300;
+const width = 563;
+const height = 306;
 
 const wallSize = 20;
 
-const ballRadius = 10;
+const ballRadius = 9.5;
 const ballColors = [
     "#FFD700", // yellow
     "#0000FF", // blue
@@ -14,10 +14,10 @@ const ballColors = [
     "#8B0000", // brown
 ];
 
-const holeRadius = ballRadius * 1.5;
+const holeRadius = 14.25;
 
-const robotWidth = 30;
-const robotHeight = 30;
+const robotWidth = 38;
+const robotHeight = 38;
 
 const wheelHeight = robotHeight/18;
 const wheelWidth = robotWidth/1.6;
