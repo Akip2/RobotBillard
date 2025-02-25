7e92b8c3a178a879ff658b8f0d0ea27d2a4f6d94
Corentin <corentin.froger@netc.fr>
2025-02-25 15:35:36 +0100

    Added debug for real robot

--- production/js/scenarios/billardScenarioComplex.js
+++ production/js/scenarios/billardScenarioComplex.js
@@ -1,9 +1,12 @@
 import {getBalls, getHoles, getRobot} from "../elements-manager.js";
 import {MIN_ORDER_DURATION, ROBOT_MAX_SPEED} from "../brain/brain-parameters.js";
-import {getNearestBall, getNearestBallToHoles, getNearestHole, normalize, sleep} from "./scenario-functions.js";
+import {getNearestBallToHoles, normalize, sleep} from "./scenario-functions.js";
 import {createOrder, isRobotFacing, isRobotNear, moveRobotTo, turnRobot} from "../brain/brain.js";
 import {isActive} from "../index.js";
 
+export let robotDestX;
+export let robotDestY;
+export let ballPush = {x: 0, y: 0};
 
 /**
  * méthode qui permet de lancer une simulation de partie de billard
@@ -24,7 +27,7 @@ export async function startBillardScenarioComplex(socket, robotIp) {
         robot = getRobot(0);
 
         const alpha = 60;
-        
+
         if (robot !== undefined) {
             let [ballToPush, hole] = getNearestBallToHoles(holes, balls);
             //const hole = getNearestHole(holes, ballToPush)
@@ -38,10 +41,11 @@ export async function startBillardScenarioComplex(socket, robotIp) {
                 let normalizedPushVector = normalize(pushVector);
 
                 // Calculate position behind the ball
-                let robotX = ballToPush.x - alpha * normalizedPushVector.x;
-                let robotY = ballToPush.y - alpha * normalizedPushVector.y;
+                robotDestX = ballToPush.x - alpha * normalizedPushVector.x;
+                robotDestY = ballToPush.y - alpha * normalizedPushVector.y;
+                ballPush = ballToPush;
 
-                while(isActive && !isRobotNear(robotIp, robotX, robotY, 30)) {
+                while (isActive && !isRobotNear(robotIp, robotDestX, robotDestY, 30)) {
                     robot = getRobot(0);
 
                     let [ballToPush, hole] = getNearestBallToHoles(holes, balls);
@@ -52,29 +56,29 @@ export async function startBillardScenarioComplex(socket, robotIp) {
                         }
                         let normalizedPushVector = normalize(pushVector);
 
-                        robotX = ballToPush.x - alpha * normalizedPushVector.x;
-                        robotY = ballToPush.y - alpha * normalizedPushVector.y;
+                        robotDestX = ballToPush.x - alpha * normalizedPushVector.x;
+                        robotDestY = ballToPush.y - alpha * normalizedPushVector.y;
 
-                        moveRobotTo(socket, robotIp, robotX, robotY);
+                        moveRobotTo(socket, robotIp, robotDestX, robotDestY);
                     }
                     await sleep(MIN_ORDER_DURATION);
                 }
-                if(!isActive) {
+                if (!isActive) {
                     break;
                 }
 
                 turnRobot(socket, robotIp, ballToPush.x, ballToPush.y);
-                while(isActive && !isRobotFacing(robotIp, ballToPush.x, ballToPush.y)){
+                while (isActive && !isRobotFacing(robotIp, ballToPush.x, ballToPush.y)) {
                     await sleep(MIN_ORDER_DURATION);
                 }
 
-                if(!isActive) {
+                if (!isActive) {
                     break;
                 }
 
-                robotX = ballToPush.x;
-                robotY = ballToPush.y;
-                while(isActive && !isRobotNear(robotIp, robotX, robotY, 40)) {
+                robotDestX = ballToPush.x;
+                robotDestY = ballToPush.y;
+                while (isActive && !isRobotNear(robotIp, robotDestX, robotDestY, 40)) {
                     robot = getRobot(0);
 
                     if (robot !== undefined) {
